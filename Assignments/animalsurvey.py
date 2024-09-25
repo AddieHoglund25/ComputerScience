79773765ab2f34f8e50cc3fa0c8bde095ee61592
AddieHoglund25 <820139@mystma.org>
2024-09-25 09:28:24 -0500

    Animal Survey with comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,7 +1,7 @@
-animal = input ("What's your favoite animal? \n")
-like = input ("Why do you like them? \n")
-color = input ("What color are they? \n")
-fact = input ("What is a fun fact about them? \n")
-live = input ("Where do they live? \n")
-eat = input ("What do they like to eat? \n")
-print ("Your favorite animal is a " + animal + " because " + like + "." + " They live in " + live + " and " + fact + ". They are " + color)
+animal = input ("What's your favoite animal? \n") # asking user what their favorite animal is
+like = input ("Why do you like them? \n") # asking why user likes them
+color = input ("What color are they? \n") # asking color of the animal
+fact = input ("What is a fun fact about them? \n") # asking for a fun fact
+live = input ("Where do they live? \n") # asking where they are found
+eat = input ("What do they like to eat? \n") # asking what their diet is
+print ("Your favorite animal is a " + animal + " because " + like + "." + " They live in " + live + " and " + fact + ". They are " + color) # the summary of all the questions
