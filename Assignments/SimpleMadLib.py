b4854e9443f56a44f432167a3bb5ffbf8e77ff00
AddieHoglund25 <820139@mystma.org>
2024-09-27 09:24:30 -0500

    simplemadlib

---
+++ Assignments/SimpleMadLib.py
@@ -0,0 +1,22 @@
+# Once upon a time, a guy who was [name]
+# he lived in [city]
+#He ran into a [animal] at [place]
+# When he saw the [animal] he screamed [exclamation]
+#
+
+#Ask for a guy's name
+name = input ("Enter a guy's name\n>")
+
+#ask for a city
+city = input ("Enter the name of a city\n>")
+
+#ask for an animal
+animal = input ("Enter an animal\n>")
+
+#ask for a place
+place = input ("Enter a place\n>")
+
+#ask for an exclamation
+exclamation = input ("Enter an exclamation\n>")
+
+print ("Once upon a time, a guy who was " + name +  ", he lived in " + city + ". He ran into a " + animal + " at " + place + " When he saw the " + animal + " he screamed " + exclamation + ".")
---
+++ Unit 2 - Functions and Variables/functions.py
@@ -0,0 +1,9 @@
+def greet ():                   #define a new function
+    print("Hello World!")      #indented lines are "inside" the function
+
+def add(x, y):
+    print(x + y)
+
+num_1 = int(input("Whats the first number? \n>"))
+num_2 = int(input("Whats the second number\n>"))
+add (num_1, num_2)
