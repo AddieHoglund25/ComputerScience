9ef25b8f5016ed7b9b026dffdb9ec266a0b8336a
AddieHoglund25 <820139@mystma.org>
2024-09-30 09:11:26 -0500

    september 30

--- Assignments/calculatetax.py
+++
@@ -1,6 +0,0 @@
-def calculate_tax(item, price, rate):        #defining a new function
-    print ()
-
-
-
-
---
+++ Unit 2 - Functions and Variables/calculatetax.py
@@ -0,0 +1,4 @@
+def calculate_tax(item, price, rate):        #defining a new function
+    print (price * rate)                        #printing the price * rate parameter
+
+calculate_tax("pencil", 1, .06875)              #testing function out
