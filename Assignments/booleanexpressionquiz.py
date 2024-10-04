dfb5dec763db0a25c07df579acd9d2ec3775699c
AddieHoglund25 <820139@mystma.org>
2024-10-04 09:11:24 -0500

    boolean expression quiz

---
+++ Assignments/booleanexpressionquiz.py
@@ -0,0 +1,15 @@
+#boolean quiz
+answer = int(input("Give a whole number that is less than 5\n>"))
+print(answer<5)
+
+answer = int(input("Give a whole number that is less than or equal to 10\n>"))
+print(answer <= 10)
+
+answer = int(input("Give a whole number that is greater than 9\n>"))
+print(answer > 9)
+
+answer = int(input("Give a number greater than or equal to 23\n>"))
+print(answer >= 23)
+
+answer = int(input("Give a number that is equal to 7\n>"))
+print(answer == 7)
