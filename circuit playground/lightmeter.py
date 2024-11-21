542b3418e0deaca4a999fc63e4e193706ab4fba9
AddieHoglund25 <820139@mystma.org>
2024-11-21 09:30:55 -0600

    november 21

--- circuit playground/lightmeter.py
+++ circuit playground/lightmeter.py
@@ -3,23 +3,33 @@
 from adafruit_circuitplayground import cp
 
 while True:
-    if cp.light >= 30:
+    if cp.light >= 30:                      # if brightness is 30, LED 0 will turn blue
         cp.pixels[0] = (0, 0, 1)
-    if cp.light == 27:
+
+    if cp.light == 27:                      # if brightness if 27, LED 1 will turn blue
         cp.pixels[1] = (0, 0, 1)
-    if cp.light == 24:
+
+    if cp.light == 24:                      # if brightness is 24, LED 2 will turn blue
         cp.pixels[2] = (0, 0, 1)
-    if cp.light == 21:
+
+    if cp.light == 21:                      # if brightness is 21, LED 3 will turn blue
         cp.pixels[3] = (0, 0, 1)
-    if cp.light == 18:
+
+    if cp.light == 18:                      # if brightness is 18, LED 4 will turn blue
         cp.pixels[4] = (0, 0, 1)
-    if cp.light == 15:
+
+    if cp.light == 15:                      # if brightness is 15, LED 5 will turn blue
         cp.pixels[5] = (0, 0, 1)   
-    if cp.light == 12:
+
+    if cp.light == 12:                      # if brightness is 12, LED 6 will turn blue
         cp.pixels[6] = (0, 0, 1)
-    if cp.light == 9:
+
+    if cp.light == 9:                       # if brightness is 9, LED 7 will turn blue
         cp.pixels[7] = (0, 0, 1)
-    if cp.light == 6:
+
+    if cp.light == 6:                       # if brightness is 6, LED 8 will turn blue
         cp.pixels[8] = (0, 0, 1)
-    if cp.light <= 3:
-        cp.pixels.fill((0, 0, 1))    
+
+    if cp.light <= 3:                       # if brightness is 3, LED 9 will turn blue
+        cp.pixels[9] = (0, 0, 1)
+  
